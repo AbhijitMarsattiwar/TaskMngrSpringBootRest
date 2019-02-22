1587808ae82063ac2899389c063b4358659ae096
AbhijitM <abhi.mars28@gmail.com>
2019-02-22 09:18:16 +0530

    Added cross origin support

--- src/main/java/com/abhi/task/controller/TaskController.java
+++ src/main/java/com/abhi/task/controller/TaskController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import com.abhi.task.model.ParentTask;
 import com.abhi.task.model.Task;
 import com.abhi.task.service.TaskService;
 
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping("/api")
 public class TaskController {
